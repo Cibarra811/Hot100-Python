9918f08976860d2d76e66932754ed1b934671645
Cibarra811 <97955086+Cibarra811@users.noreply.github.com>
2023-07-02 17:06:39 +0700

    Update 01_Home.py

--- pages/01_Home.py
+++ pages/01_Home.py
@@ -1,2 +1,16 @@
 import streamlit as st 
 
+st.title("Billboard Hot 100 Analysis and Machine Learning in Python from (1958-2020)")
+
+st.write("""
+## Welcome to the Billboard Hot 100 Analysis and Machine Learning Project. 
+## This is a personal project by me, Cesario Angel Ibarra, 
+### a musician and composer with 30 years of experience and four published albums on all streaming platforms. 
+### I have always been fascinated by the mathematical nature of music, how frequency, time and vibration 
+### make up the fabric of the universe and how music is a manifestation of this phenomenon. 
+### In this project, I will use data analysis and machine learning techniques to explore the similarities and differences
+### among the songs that make it to the Hot 100, the most popular songs in the US. 
+### I will examine how these songs share the same musical structure, such as key signature, duration, valence, etc. 
+### My goal is to demonstrate how music can be understood and created using scientific methods, 
+### as well as to appreciate the diversity and creativity of musical expression. 
+         """)
